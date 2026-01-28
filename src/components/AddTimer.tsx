f6f1ca8b024ae7e08c738482eb417f47e926c910
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-28 19:49:18 +0530

    Created the AddTimer form component for creating and adding a timer

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -1,7 +1,26 @@
-import type { FC } from "react";
+import { useRef, type FC } from "react";
+import Form, { type FormApi } from "../ui/Form";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
 
 const AddTimer: FC = function () {
-  return <div></div>;
+  const form = useRef<FormApi>(null);
+
+  function handleSaveTimer(data: unknown) {
+    const extractedData = data as { name: string; duration: string };
+    console.log(extractedData);
+    form.current?.clear();
+  }
+
+  return (
+    <Form ref={form} onSave={handleSaveTimer} id="add-timer">
+      <Input type="text" label="Name" id="name" name="name" />
+      <Input type="number" label="Duration" id="duration" name="duration" />
+      <p>
+        <Button>Add Timer</Button>
+      </p>
+    </Form>
+  );
 };
 
 export default AddTimer;
