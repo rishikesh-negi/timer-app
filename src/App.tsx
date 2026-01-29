917eae25ba00a4966dc4c0297b43ed14f85f8ae2
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-29 19:45:09 +0530

    Wrapped the components needing access to the context with the context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import AddTimer from "./components/AddTimer";
 import Header from "./components/Header";
 import Timers from "./components/Timers";
+import TimersContextProvider from "./store/timers-context";
 
 export default function App() {
   return (
-    <>
+    <TimersContextProvider>
       <Header />
       <main>
         <AddTimer />
         <Timers />
       </main>
-    </>
+    </TimersContextProvider>
   );
 }
