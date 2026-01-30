2186fa4681e13b4ebb88ca0a8dd5a2cc0cc1e405
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-30 19:50:39 +0530

    Added the component's JSX and used Timer context values for conditional rendering and button functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
-import type { FC } from "react";
+import { type FC } from "react";
+import { useTimersContext } from "../store/timers-context";
+import Button from "../ui/Button";
 
 const Header: FC = function () {
-  return <div></div>;
+  const { isRunning, stopTimers, startTimers } = useTimersContext();
+
+  return (
+    <header>
+      <h1>React Timer</h1>
+
+      <Button onClick={isRunning ? stopTimers : startTimers}>
+        {isRunning ? "Stop" : "Start"} Timers
+      </Button>
+    </header>
+  );
 };
 
 export default Header;
