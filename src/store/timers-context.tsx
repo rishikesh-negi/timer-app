19e17fd16dbd3209f7edb334feec6d957d405f40
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-30 19:43:11 +0530

    Created a reducer to manage context state and a custom hook to provide context value to subscribers

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,8 +1,8 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useContext, useReducer, type ReactNode } from "react";
 
-type Timer = {
+export type Timer = {
   name: string;
-  duration: string;
+  duration: number;
 };
 
 // Type for the timers' state:
@@ -11,31 +11,89 @@ type TimersState = {
   timers: Timer[];
 };
 
-// Type for the timers' state combined with state-manipulating functions. To be used as the type argument of createContext:
+// Type for the timers context value. Used as the first type argument of createContext:
 type TimersContextValue = TimersState & {
   addTimer: (timerData: Timer) => void;
   startTimers: () => void;
   stopTimers: () => void;
 };
 
-const TimersContext = createContext<TimersContextValue | null>(null);
-
 type TimersContextProviderProps = {
   children: ReactNode;
 };
 
+// Initial state for the timers reducer:
+const initialState: TimersState = {
+  isRunning: false,
+  timers: [],
+};
+
+/*
+// Type for the reducer actions for the timers reducer:
+type TimersAction = {
+  type: "ADD_TIMER" | "START_TIMERS" | "STOP_TIMERS";
+  payload?: unknown;
+};
+// This type for the reducer actions allows for accessing the "payload" property even for actions that don't need it. The "payload" property can also be "null" since it needs to be an optional property. So, to use the payload, we would have to do some tedious type narrowing or use an "as" or non-null assertion, none of which are ideal. Additionally, the "unknown" type is not suitable for the payload property. We must define the precise type in most cases. However, if we do define the precise type, we might have to change or update it many times as and when more actions get added to the reducer function. Therefore, this type is not ideal.
+*/
+
+// A better way to type the reducer actions using discriminated unions:
+type StartTimersAction = { type: "START_TIMERS" };
+type StopTimersAction = { type: "STOP_TIMERS" };
+type AddTimerAction = { type: "ADD_TIMER"; payload: Timer };
+
+type TimersAction = StartTimersAction | StopTimersAction | AddTimerAction;
+
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+function timersReducer(state: TimersState, action: TimersAction): TimersState {
+  switch (action.type) {
+    case "ADD_TIMER":
+      return {
+        ...state,
+        timers: [
+          ...state.timers,
+          { name: action.payload.name, duration: action.payload.duration },
+        ],
+      };
+    case "START_TIMERS":
+      return { ...state, isRunning: true };
+    case "STOP_TIMERS":
+      return { ...state, isRunning: false };
+    default:
+      throw new Error(`Unknown action received: ${action as never}`);
+  }
+}
+
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
-    addTimer(timerData) {},
-    startTimers() {},
-    stopTimers() {},
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
+    addTimer(timerData) {
+      dispatch({ type: "ADD_TIMER", payload: timerData });
+    },
+    startTimers() {
+      dispatch({ type: "START_TIMERS" });
+    },
+    stopTimers() {
+      dispatch({ type: "STOP_TIMERS" });
+    },
   };
 
   return (
     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
   );
+}
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null)
+    throw new Error("Context was used outside its provider!");
+
+  return timersCtx;
 }
