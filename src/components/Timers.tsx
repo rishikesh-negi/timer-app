b454f8370a0151acebb4178ebfaa8310602d0ee8
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-30 19:46:41 +0530

    Used Timer context to fetch and render all added timers

--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,7 +1,19 @@
 import type { FC } from "react";
+import { useTimersContext } from "../store/timers-context";
+import Timer from "./Timer";
 
 const Timers: FC = function () {
-  return <div></div>;
+  const { timers } = useTimersContext();
+
+  return (
+    <ul>
+      {timers.map((timer) => (
+        <li key={`${timer.name}${timer.duration}${Math.random()}`}>
+          <Timer name={timer.name} duration={timer.duration} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Timers;
