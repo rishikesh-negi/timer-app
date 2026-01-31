ee6c3b3ce34c5368b708ebf6788491bdbe6627db
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-31 18:40:55 +0530

    Added plugins and rules for linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks, { rules } from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "typescript-eslint": tseslint,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,7 +24,9 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      "typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": "warn",
     },
   },
 ]);
